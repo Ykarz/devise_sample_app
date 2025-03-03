817b7c5bd2681ac3597cdd8922040151df7df927
Ykarz <blhlc0820@gmail.com>
2025-03-03 16:58:17 +0900

    [update]ログイン画面のスタイル修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # deviseで作成したコントローラーへのルーティング
+  devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout' }
 
   # Defines the root path route ("/")
   # root "posts#index"
