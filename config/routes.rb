c47272cc9242a3d78558f037b1b0229023ecd999
Ykarz <blhlc0820@gmail.com>
2025-03-09 23:18:55 +0900

    [add]ログイン・ログアウト機能の実装完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
+  root "home#index"
+
   # deviseで作成したコントローラーへのルーティング
   devise_for :users,
-             path: "",
-             path_names: { sign_in: 'login', sign_out: 'logout' }
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+            path: "",
+            path_names: { sign_in: 'login', sign_out: 'logout' }
 end
